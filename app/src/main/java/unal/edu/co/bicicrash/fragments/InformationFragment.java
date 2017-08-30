b510ca19b20b66decd603a2e0895f215e386d9fa
MiguelPC <miatorresch@unal.edu.co>
2017-08-30 00:37:08 -0500

    - Se crea el formulario que guarda los datos personales del usuario

--- app/src/main/java/unal/edu/co/bicicrash/fragments/InformationFragment.java
+++ app/src/main/java/unal/edu/co/bicicrash/fragments/InformationFragment.java
@@ -1,12 +1,17 @@
 package unal.edu.co.bicicrash.fragments;
 
 import android.content.Context;
+import android.media.MediaCodec;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.regex.Pattern;
 
 import unal.edu.co.bicicrash.R;
 
@@ -22,7 +27,22 @@ public class InformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_information, container, false);
+        return view;
+    }
+
+    private boolean validarNombre(String nombre){
+        Pattern patron = Pattern.compile("^[a-zA-Z ]+$");
+        return patron.matcher(nombre).matches() || nombre.length() > 30;
+
+
+    }
+
+    private boolean validadTelefono(String telefono){
+        return Patterns.PHONE.matcher(telefono).matches();
+    }
+    private boolean esCorreoValido(String correo){
+        return Patterns.EMAIL_ADDRESS.matcher(correo).matches();
     }
 
 
