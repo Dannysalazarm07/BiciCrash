774644a2141b5450cfaaabbd162f1e8b817cd7bf
Daniel Salazar <dannysalazarm07@gmail.com>
2017-10-31 17:42:56 -0500

    facebook api full

--- app/src/main/java/unal/edu/co/bicicrash/Activities/SahreOnFb.java
+++ app/src/main/java/unal/edu/co/bicicrash/Activities/SahreOnFb.java
@@ -1,12 +1,20 @@
 package unal.edu.co.bicicrash.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
 
 import unal.edu.co.bicicrash.R;
@@ -14,13 +22,59 @@ import unal.edu.co.bicicrash.R;
 public class SahreOnFb extends FragmentActivity {
     CallbackManager callbackManager;
     ShareDialog shareDialog;
+    Intent intent;
+    //final Button button2 = (Button) findViewById(R.id.button2);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sahre_on_fb);
         callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
-        // this part is optional
-        //shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() { ... });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.button2).performClick();
+            }
+        }, 5000);
+
     }
+
+    public void publicar(View view){
+        //intent = new Intent(this, SahreOnFb.class);
+        try {
+
+            if (ShareDialog.canShow(ShareLinkContent.class)) {
+//                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+//                        //.setContentTitle("Ayudaaa me accidenté")
+//                        .setContentDescription("estoy en ... : ----, esto es una prueba")
+//                        //.setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+//                        .setQuote("Hola bicicrash 2017")
+//                        .build();
+//                shareDialog.show(linkContent);
+//                //startActivity(intent);
+
+                ShareLinkContent content = new ShareLinkContent.Builder()
+                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                        .setQuote("Prueba BiciCrash")
+                        .build();
+                shareDialog.show(content, ShareDialog.Mode.AUTOMATIC);
+
+            }
+        }catch (Exception e){
+            Toast.makeText(this, "algo paso: "+e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
